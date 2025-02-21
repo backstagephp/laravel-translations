27b8a69f607aacc4c109b6cd2df9ef00e220b0f8
Mark van Eijk <mark@vormkracht10.nl>
2025-02-21 15:22:04 +0100

    Update Language.php

--- src/Models/Language.php
+++ src/Models/Language.php
@@ -5,10 +5,13 @@ namespace Backstage\Translations\Laravel\Models;
 use Backstage\Translations\Laravel\Observers\LanguageObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 #[ObservedBy(LanguageObserver::class)]
 class Language extends Model
 {
+    use HasFactory;
+
     protected $table = 'languages';
 
     protected $primaryKey = 'code';
@@ -28,6 +31,15 @@ class Language extends Model
     public static function default(): ?Language
     {
         return static::firstWhere('default', 1);
+    }
+    
+    protected static function newFactory()
+    {
+        $package = Str::before(get_called_class(), 'Models\\');
+        $modelName = Str::after(get_called_class(), 'Models\\');
+        $path = $package . 'Database\\Factories\\' . $modelName . 'Factory';
+
+        return $path::new();
     }
 
     public function getLanguageCodeAttribute()
