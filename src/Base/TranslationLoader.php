aa0c75806f2e08a3d46a2db7844131d632226e42
Mark van Eijk <mark@vaneijk.co>
2025-02-27 12:36:31 +0100

    wip

--- src/Base/TranslationLoader.php
+++ src/Base/TranslationLoader.php
@@ -8,13 +8,15 @@ use Illuminate\Translation\FileLoader;
 
 class TranslationLoader extends FileLoader
 {
-    public function load($locale, $group, $namespace = null)
+    public function load($locale, $group, $namespace = null): array
     {
         $fileTranslations = parent::load($locale, $group, $namespace);
 
+        dd($this->loadPaths($this->paths, $locale, $group));
+
         if (
             ! Schema::hasTable((new Translation)->getTable()) ||
-            ! is_null($namespace) && $namespace !== '*'
+            (! is_null($namespace) && $namespace !== '*')
         ) {
             return $fileTranslations;
         }
@@ -22,11 +24,8 @@ class TranslationLoader extends FileLoader
         return array_replace_recursive($fileTranslations, $this->getTranslationsFromDatabase($locale, $group, $namespace));
     }
 
-    protected function getTranslationsFromDatabase(
-        string $locale,
-        string $group,
-        string|null $namespace = null
-    ): array {
+    protected function getTranslationsFromDatabase(string $locale, string $group, string|null $namespace = null): array
+    {
         return Translation::select('key', 'text')
             ->where('group', $group)
             ->where('namespace', $namespace)
