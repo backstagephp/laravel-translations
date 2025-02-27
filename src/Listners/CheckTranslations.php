90730fa9c20ddea00db26a4d98b0b98022696e2d
Manoj Hortulanus <mdhortulanus@gmail.com>
2025-02-27 11:13:21 +0100

    Add checking if lang code is change, if changed dispatch translation job.

--- src/Listners/CheckTranslations.php
+++ src/Listners/CheckTranslations.php
@@ -4,6 +4,7 @@ namespace Backstage\Translations\Laravel\Listners;
 
 use Backstage\Translations\Laravel\Events\LanguageDeleted;
 use Backstage\Translations\Laravel\Events\LanguageUpdated;
+use Backstage\Translations\Laravel\Jobs\TranslateKeys;
 use Backstage\Translations\Laravel\Models\Translation;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -11,8 +12,12 @@ class CheckTranslations implements ShouldQueue
 {
     public function handle(LanguageUpdated $event)
     {
-        Translation::where('code', $event->language->getOriginal('code'))
-            ->get()
-            ->each(fn($translation) => $translation->update(['code' => $event->language->getAttribute('code')]));
+        if ($event->language->wasChanged('code')) {
+            Translation::where('code', $event->language->getOriginal('code'))
+                ->get()
+                ->each(fn($translation) => $translation->update(['code' => $event->language->getAttribute('code')]));
+
+            TranslateKeys::dispatch($event->language);
+        }
     }
 }
