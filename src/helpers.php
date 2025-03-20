4646da84a0c16a49a3d06484a9ef067f4ccce08f
Mark van Eijk <mark@vormkracht10.nl>
2025-03-20 11:43:42 +0100

    Create helpers.php

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+function getLocalizedCountryName(string $code, ?string $locale = null): string
+{
+    $code = strtolower(explode('-', $code)[1] ?? $code);
+
+    return Locale::getDisplayRegion('-' . $code, $locale ?? app()->getLocale());
+}
+
+function getLocalizedLanguageName(string $code, ?string $locale = null): string
+{
+    $code = strtolower(explode('-', $code)[0]);
+
+    return Locale::getDisplayLanguage($code, $locale ?? app()->getLocale());
+}
