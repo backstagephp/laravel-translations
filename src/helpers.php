1224774a9f3635cb659fc232ec47ff02e9275b0b
markvaneijk <1925388+markvaneijk@users.noreply.github.com>
2025-03-20 10:43:57 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -4,7 +4,7 @@ function getLocalizedCountryName(string $code, ?string $locale = null): string
 {
     $code = strtolower(explode('-', $code)[1] ?? $code);
 
-    return Locale::getDisplayRegion('-' . $code, $locale ?? app()->getLocale());
+    return Locale::getDisplayRegion('-'.$code, $locale ?? app()->getLocale());
 }
 
 function getLocalizedLanguageName(string $code, ?string $locale = null): string
