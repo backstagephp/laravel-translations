a6b21e9f12a33afff1713d7b6d8845bace62a4c0
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-04-28 14:01:42 +0200

    Correct vendor !! Important commit

--- src/Drivers/AITranslator.php
+++ src/Drivers/AITranslator.php
@@ -2,8 +2,8 @@
 
 namespace Backstage\Translations\Laravel\Drivers;
 
+use Prism\Prism\Prism;
 use Backstage\Translations\Laravel\Contracts\TranslatorContract;
-use EchoLabs\Prism\Prism;
 
 class AITranslator implements TranslatorContract
 {
@@ -12,8 +12,8 @@ class AITranslator implements TranslatorContract
         $response = Prism::text()
             ->using(config('translations.translators.drivers.ai.provider'), config('translations.translators.drivers.ai.model'))
             ->withSystemPrompt(config('translations.translators.ai.system_prompt'))
-            ->withPrompt('Translate the following text to '.$targetLanguage.': '.$text)
-            ->generate();
+            ->withPrompt('Translate the following text to ' . $targetLanguage . ': ' . $text)
+            ->asText();
 
         return $response->text;
     }
