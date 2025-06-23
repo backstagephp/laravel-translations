7fa9180180c75dcd8698e2912eb1a95ffe2667ac
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-06-23 14:04:40 +0200

    wip

--- src/Drivers/DeepLTranslator.php
+++ src/Drivers/DeepLTranslator.php
@@ -2,8 +2,9 @@
 
 namespace Backstage\Translations\Laravel\Drivers;
 
-use Backstage\Translations\Laravel\Contracts\TranslatorContract;
+use Locale;
 use DeepL\DeepLClient;
+use Backstage\Translations\Laravel\Contracts\TranslatorContract;
 
 class DeepLTranslator implements TranslatorContract
 {
@@ -13,8 +14,34 @@ class DeepLTranslator implements TranslatorContract
 
         $deeplClient = new DeepLClient($authkey, config('translations.translators.drivers.deep-l.options', []));
 
+        $targetLanguage = static::normalizeLanguageCode($targetLanguage);
+
         $result = $deeplClient->translateText($text, null, $targetLanguage);
 
         return $result->text;
+    }
+
+    protected static function normalizeLanguageCode(string $lang): string
+    {
+        $canonical = Locale::canonicalize($lang);
+
+        $language = Locale::getPrimaryLanguage($canonical);
+
+        $region = Locale::getRegion($canonical);
+
+        if (! $region) {
+            if ($language === 'en') {
+                $region = 'GB';
+            } elseif ($language === 'pt') {
+                $region = 'PT';
+            } else {
+                $region = strtoupper($language);
+            }
+        }
+
+        return Locale::composeLocale([
+            'language' => $language,
+            'region' => $region,
+        ]);
     }
 }
