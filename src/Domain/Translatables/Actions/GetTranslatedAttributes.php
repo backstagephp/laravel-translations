214b54be2f0262d280e5b263c243bb888867a5f9
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-07-05 13:50:29 +0200

    wip

--- src/Domain/Translatables/Actions/GetTranslatedAttributes.php
+++ src/Domain/Translatables/Actions/GetTranslatedAttributes.php
@@ -11,12 +11,18 @@ class GetTranslatedAttributes
     use AsAction;
 
     /**
-     * @param  HasTranslatableAttributes|Model  $model
+     * @param HasTranslatableAttributes|Model  $model
      */
     public function handle($model, string $locale): array
     {
+        /**
+         * @var array $translatableAttributes
+         */
         $translatableAttributes = $model->getTranslatableAttributes();
 
+        /**
+         * @var array $translatedAttributes
+         */
         $translatedAttributes = collect($translatableAttributes)->mapWithKeys(function ($attribute) use ($model, $locale) {
             return $model->getTranslatedAttribute($attribute, $locale) !== null
                 ? [$attribute => $model->getTranslatedAttribute($attribute, $locale)]
