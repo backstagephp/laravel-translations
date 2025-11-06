3bd6cfc6b04733eca55c9f0e9656749d0804dcb1
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-11-06 16:50:01 +0100

    Remove enums

--- src/Models/LanguageRuleCondition.php
+++ src/Models/LanguageRuleCondition.php
@@ -2,14 +2,10 @@
 
 namespace Backstage\Translations\Laravel\Models;
 
-use Backstage\Translations\Laravel\Enums\LanguageRuleConditionType;
 use Backstage\Translations\Laravel\Observers\LanguageRuleConditionObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @property LanguageRuleConditionType $type
- */
 #[ObservedBy(LanguageRuleConditionObserver::class)]
 class LanguageRuleCondition extends Model
 {
@@ -24,7 +20,7 @@ class LanguageRuleCondition extends Model
     ];
 
     protected $casts = [
-        'type' => LanguageRuleConditionType::class,
+        'type' => 'string',
         'value' => 'string',
         'multiple_value' => 'array',
     ];
@@ -36,6 +32,50 @@ class LanguageRuleCondition extends Model
 
     public function getTextualQuery(): string
     {
-        return $this->type->buildTextualQuery($this->key, $this->value ?? $this->multiple_value);
+        $key = $this->key;
+        $value = null;
+        if ($this->type === 'must' || $this->type === 'must_not') {
+            $value = $this->value;
+        }
+
+        if ($this->type === 'must_multiple' || $this->type === 'must_not_multiple') {
+            $value = $this->multiple_value;
+        }
+
+        if ($value === null) {
+            report(new \Exception('Value is null for key: ' . $key . ' and type: ' . $this->type. ' and id: ' . $this->id));
+            return '';
+        }
+        
+
+        $resultingQuery = [];
+
+        if (is_array($value) && count($value) > 0 || ! is_array($value) && $value !== '' || $value !== null) {
+            $resultingQuery[] = 'The following rules must be applied while translating the text:';
+        }
+
+        if ($this->type === 'must' || $this->type === 'must_not') {
+            if ($this->type === 'must') {
+                $resultingQuery[] = "{$key} must translate to '{$value}'";
+            }
+
+            if ($this->type === 'must_not') {
+                $resultingQuery[] = "{$key} must not translate to '{$value}'";
+            }
+        }
+
+        if ($this->type === 'must_multiple') {
+            $values = $value;
+
+            $resultingQuery[] = "{$key} must translate to one of these options: '".implode("', '", $values)."'";
+        }
+
+        if ($this->type === 'must_not_multiple') {
+            $values = $value;
+
+            $resultingQuery[] = "{$key} must not translate to any of these options: '".implode("', '", $values)."'";
+        }
+
+        return implode("\n", $resultingQuery);
     }
 }
--- src/Observers/LanguageRuleConditionObserver.php
+++ src/Observers/LanguageRuleConditionObserver.php
@@ -2,7 +2,6 @@
 
 namespace Backstage\Translations\Laravel\Observers;
 
-use Backstage\Translations\Laravel\Enums\LanguageRuleConditionType;
 use Backstage\Translations\Laravel\Models\LanguageRuleCondition;
 
 class LanguageRuleConditionObserver
@@ -10,22 +9,9 @@ class LanguageRuleConditionObserver
     public function saving(LanguageRuleCondition $condition)
     {
         $type = $condition->getAttribute('type');
-        $originalType = $condition->getOriginal('type');
 
-        $singleTypes = [
-            LanguageRuleConditionType::MUST,
-            LanguageRuleConditionType::MUST_NOT,
-        ];
-
-        $multipleTypes = [
-            LanguageRuleConditionType::MUST_MULTIPLE,
-            LanguageRuleConditionType::MUST_NOT_MULTIPLE,
-        ];
-
-        if (in_array($type, $singleTypes, true)) {
+        if ($type === 'must' || $type === 'must_not') {
             $condition->multiple_value = [];
         }
-
-        // dd($condition->toArray());
     }
 }
