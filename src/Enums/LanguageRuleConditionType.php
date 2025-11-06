82c20e40a73a721635ffd510a4e2f4f4ddfb7653
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-11-06 09:32:23 +0100

    Rename cases

--- src/Enums/LanguageRuleConditionType.php
+++ src/Enums/LanguageRuleConditionType.php
@@ -6,14 +6,14 @@ enum LanguageRuleConditionType: string
 {
     case MUST = 'must';
     case MUST_NOT = 'must_not';
-    case MUST_REPLACE = 'must_replace';
-    case MUST_NOT_REPLACE = 'must_not_replace';
+    case MUST_MULTIPLE = 'MUST_MULTIPLE';
+    case MUST_NOT_MULTIPLE = 'MUST_NOT_MULTIPLE';
 
     public function mutateValue(string $value): string
     {
         return match ($this) {
             self::MUST, self::MUST_NOT => $value,
-            self::MUST_NOT_REPLACE, self::MUST_REPLACE => json_decode($value, true),
+            self::MUST_NOT_MULTIPLE, self::MUST_MULTIPLE => json_decode($value, true),
         };
     }
 
