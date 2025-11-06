b6cfcbe3240e9d6cbef4dd1c1f2e88656f44de45
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-11-06 10:03:05 +0100

    Update value structure

--- src/Models/LanguageRuleCondition.php
+++ src/Models/LanguageRuleCondition.php
@@ -2,12 +2,15 @@
 
 namespace Backstage\Translations\Laravel\Models;
 
-use Backstage\Translations\Laravel\Enums\LanguageRuleConditionType;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Backstage\Translations\Laravel\Enums\LanguageRuleConditionType;
+use Backstage\Translations\Laravel\Observers\LanguageRuleConditionObserver;
 
 /**
  * @property LanguageRuleConditionType $type
  */
+#[ObservedBy(LanguageRuleConditionObserver::class)]
 class LanguageRuleCondition extends Model
 {
     protected $table = 'language_rules_conditions';
@@ -17,17 +20,15 @@ class LanguageRuleCondition extends Model
         'key',
         'type',
         'value',
+        'multiple_value'
     ];
 
     protected $casts = [
         'type' => LanguageRuleConditionType::class,
+        'value' => 'string',
+        'multiple_value' => 'array',
     ];
-
-    public function getValueAttribute()
-    {
-        return $this->type->mutateValue($this->value);
-    }
-
+   
     public function languageRule()
     {
         return $this->belongsTo(LanguageRule::class);
