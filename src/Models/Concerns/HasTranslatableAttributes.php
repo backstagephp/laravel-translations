b530ada78a5f9d68ae5f78b5c8271b269f9b7154
markvaneijk <1925388+markvaneijk@users.noreply.github.com>
2026-06-08 14:27:11 +0000

    Fix styling

--- src/Models/Concerns/HasTranslatableAttributes.php
+++ src/Models/Concerns/HasTranslatableAttributes.php
@@ -181,9 +181,9 @@ trait HasTranslatableAttributes
         );
     }
 
-    public function getTranslatableAttributeRulesFor(string $attribute): array | string
+    public function getTranslatableAttributeRulesFor(string $attribute): array|string
     {
-        $methodName = 'getTranslatableAttributeRulesFor' . str($attribute)->studly();
+        $methodName = 'getTranslatableAttributeRulesFor'.str($attribute)->studly();
 
         if (! method_exists($this, $methodName)) {
             return '*';
